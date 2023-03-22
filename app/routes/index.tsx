141ac0fd91f59090fd049c0e84457525bb695c78
Skyla Reiner <sysreiner@gmail.com>
2023-03-22 13:48:26 -0700

    more styling fixes

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -15,18 +15,18 @@ export default function Index() {
         <div className="container absolute top-0 left-0 flex flex-col items-center h-[100vh] bg-dark-blue bg-clip-content">
           <Navbar />
           <div className="static h-full max-w-md md:px-4 flex flex-col justify-center items-center z-10 mx-4 -mt-[12vh] space-y-3">
-            <span className="flex container items-center ">
+            <span className="flex container items-center">
               <p className="font-rubik text-md uppercase text-white">coming soon</p><div className="ml-1 w-2.5 bg-white h-0.5"></div>
             </span>
             <h1 className="-mt-1 font-rubik font-bold text-gray-5 text-4xl whitespace-normal">Sign up to stay updated</h1>
             <div className="container" id="mc_embed_signup">
               <form action="https://choosefleet.us21.list-manage.com/subscribe/post?u=aa9300a32dde53976a2364262&amp;id=046defb154&amp;f_id=00a4aae1f0" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="flex flex-col" target="_self">
                 <div className="mt-3 inline-flex sm:h-7 h-9 border border-gray-60" id="mc_embed_signup_scroll">
-                  <div className="flex w-full">
-                    <input type="email" value={email} onChange={(e) => handleEmailChange(e.target.value)} placeholder="Enter your email here" name="EMAIL" className="focus:outline-none focus:bg-white focus:text-gray-80 bg-white bg-opacity-90 w-full text-gray-50 pl-3 font-rubik text-xs" id="mce-EMAIL" required />
+                  <div className="flex w-full border-none">
+                    <input type="email" value={email} onChange={(e) => handleEmailChange(e.target.value)} placeholder="Enter your email here" name="EMAIL" className="focus:outline-none focus:bg-white focus:text-gray-80 bg-gray-5 bg-opacity-90 w-full text-gray-50 pl-3 font-rubik text-xs" id="mce-EMAIL" required />
                     <span id="mce-EMAIL-HELPERTEXT" className="helper_text"></span>
                   </div>
-                  <input type="submit" className="active:bg-gray-90 active:text-gray-50 cursor-pointer bg-gray-80 text-gray-5 font-rubik text-xs px-4" value="Submit" name="Submit" id="mc-embedded-subscribe" />
+                  <input type="submit" className="active:bg-gray-90 active:text-gray-50 cursor-pointer bg-gray-80 text-gray-5 font-rubik text-xs px-4 border-none" value="Submit" name="Submit" id="mc-embedded-subscribe" />
                 </div>
               </form>
             </div>
